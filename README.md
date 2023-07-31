8fb4f74c479a92bbc24a86e57b022592b9dc0589
Derek Wan <d.wan@icloud.com>
2023-07-31 11:13:19 +0900

    Add `animal-farm` (#2)

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # React - The Full Course (Fireship.io)
 
 - https://fireship.io/courses/react/
+- https://github.com/fireship-io/react-course
 
 ## Getting started
 
---
+++ animal-farm/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
