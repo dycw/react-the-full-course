0e1c41848b978bc9d0fcbdf52def00cfab151f67
Derek Wan <d.wan@icloud.com>
2023-07-31 11:09:28 +0900

    Add `animal-farm`

--- src/app/hooks.ts
+++
@@ -1,9 +0,0 @@
-import {
-  useDispatch,
-  useSelector,
-  type TypedUseSelectorHook,
-} from "react-redux";
-import type { Dispatch, State } from "./store";
-
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
-export const useAppDispatch = () => useDispatch<Dispatch>();
